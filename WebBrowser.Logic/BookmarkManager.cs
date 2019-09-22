e837f9ea053ea8f00a61d483b23c328948bff2c0
Ander Lin <lzl0078@tigermail.auburn.edu>
2019-09-21 19:08:40 -0500

    Add a new database and add the following tables:  History (columns: Id, URL, Title, Date)

---
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBrowser.Data.BookmarksDataSetTableAdapters;
+
+namespace WebBrowser.Logic
+{
+    public class BookmarkManager
+    {
+        public static void AddItem(BookmarkItem item)
+        {
+            var adapter = new BookmarksTableAdapter();
+            adapter.Insert(item.URL, item.Title);
+        }
+
+        public static List<BookmarkItem> GetItems()
+        {
+            var adapter = new BookmarksTableAdapter();
+            var results = new List<BookmarkItem>();
+            var rows = adapter.GetData();
+
+            foreach (var row in rows)
+            {
+                var item = new BookmarkItem();
+                item.URL = row.URL;
+                item.Title = row.Title;
+
+
+                results.Add(item);
+            }
+            return results;
+
+        }
+    }
+}
